4a36378d4e7cf430e3c6f1187898e9316c9b55c1
Sess254 <wamaritemoses@gmail.com>
2023-08-01 19:49:08 +0300

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-# ruby script that
-# outputs senders phone number
-# receivers phone number
-# flags used
-
-from = ARGV[0].scan(/from:(.*?)\]/)
-to = ARGV[0].scan(/to:(.*?)\]/)
-flags = ARGV[0].scan(/flags:(.*?)\]/)
-puts [from, to, flag].join(",")
+puts [ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\].join(",")
