4c8f056a38f0308fac109f15b2dff8b5746c4cd8
Sess254 <wamaritemoses@gmail.com>
2023-11-07 14:07:48 +0300

    Top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""  function that queries the Reddit API and prints the titles
+     of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+            "User-Agent": "Reddit Top Ten Posts (by your_username)"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        if "data" in data and "children" in data["data"]:
+            posts = data["data"]["children"][:10]
+            for post in posts:
+                title = post["data"]["title"]
+                print(title)
+    else:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten()
