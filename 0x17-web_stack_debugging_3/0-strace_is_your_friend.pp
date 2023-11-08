8ae187c86c309272257908dc272093c941c253b7
Sess254 <wamaritemoses@gmail.com>
2023-11-08 18:57:44 +0300

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-0-strace_is_your_friend.pp
+# Fix & automate it using strace
+exec { 'fix phpp':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/bin']
+}
